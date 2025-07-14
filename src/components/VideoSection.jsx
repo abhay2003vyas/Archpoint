3b0ece374aab86f357e30c2d0f4f1bf312c1dbf6
Abhay Vyas <vyas0994@gmail.com>
2025-07-14 15:58:24 +0530

    update video section

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -3,10 +3,12 @@ import logo from '../assets/logo.jpg';
 import videoBg from '../assets/video.mp4';
 
 const VideoSection = () => (
-  <section className="relative h-[40vh] md:h-[100vh] flex flex-col items-center justify-center bg-black overflow-hidden">
+  <section
     
-    {/* Background video */}
+    className="relative h-[100vh] flex flex-col items-center justify-center bg-black overflow-hidden"
+  >
     <video
+    id="video-section"
       className="absolute inset-0 w-full h-full object-cover opacity-80 z-0"
       src={videoBg}
       autoPlay
@@ -14,16 +16,6 @@ const VideoSection = () => (
       muted
       playsInline
     />
-
-    {/* Logo layered above the video */}
-    <div className="mb-6 flex justify-center z-10">
-      <img
-        src={logo}
-        alt="Arch Point Consultants Pvt. Ltd."
-        className="h-28 w-auto object-contain rounded-md p-2"
-      />
-    </div>
-
   </section>
 );
 
