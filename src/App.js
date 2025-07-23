30e541e04e5a5ddd9532b316a390b958c1d1eb3b
Abhay Vyas <vyas0994@gmail.com>
2025-07-23 16:17:15 +0530

    modify app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import CompanyIntro from './components/CompanyIntro';
 import VideoSection from './components/VideoSection';
@@ -9,20 +10,33 @@ import ClientLogos from './components/ClientLogos';
 import ClientTestimonials from './components/ClientTestimonials';
 import Footer from './components/Footer';
 import AwardsRecognition from './components/AwardsRecognition';
+import Projects from './pages/Architectural_Design'; // <-- new route page
+import PMC from './pages/PMCPage';
+
 import '@fontsource/montserrat';
 import '@fontsource/merriweather';
 import './index.css';
+
+const Home = () => (
+  <>
+    <VideoSection />
+    <AwardsRecognition />
+    <FeaturedProjects />
+    <ClientLogos />
+    <ClientTestimonials />
+    <Footer />
+  </>
+);
 
 function App() {
   return (
     <div className="font-sans bg-beige text-dark">
       <Navbar />
-      <VideoSection />
-      <AwardsRecognition/>
-      <FeaturedProjects/>
-      <ClientLogos />
-      <ClientTestimonials />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/pmc" element={<PMC />} />
+      </Routes>
     </div>
   );
 }
